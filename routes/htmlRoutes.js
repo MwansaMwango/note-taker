8bd260ea3d1cd9c1a19553dcc03583674ae2bd0b
Mwansa <Mwansad.mwango@gmail.com>
2020-05-03 23:34:03 +0800

    Removed wildcard route to avoid masking api routes in seperate folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const path = require("path");
 
 
 // ===============================================================================
@@ -17,10 +17,11 @@ module.exports = function(app) {
 
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
+    
   });
 
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  // // If no matching route is found default to home
+  // app.get("/*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/index.html"));
+  // });
 };
