2b6809cd2d26792f80addbc662720392448f3d34
Mwansa <Mwansad.mwango@gmail.com>
2020-05-06 23:56:00 +0800

    updates server js. variables.

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
+// global.nextNoteID ='';
+
+  global.count = 0;
 
 const express = require("express");
 const path = require('path');
+
+
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
